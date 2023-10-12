2b0cc6db1f7fe308e568026fbd4830ba0d93e087
jatin <adriano23012001@gmail.com>
2023-10-12 17:22:16 +0530

    added department model

---
+++ src/models/Department.ts
@@ -0,0 +1,29 @@
+import { Model, Table, Column, DataType, ForeignKey, BelongsTo, AutoIncrement, PrimaryKey, AllowNull, HasMany } from 'sequelize-typescript';
+import UserDepartment from './UserDepartment';
+
+@Table({
+  tableName: 'department',
+  timestamps: false
+})
+export default class Department extends Model {
+  @AutoIncrement
+  @PrimaryKey
+  @Column({
+    type: DataType.INTEGER,
+  })
+  id!: number;
+
+  @AllowNull(false)
+  @Column({
+    type: DataType.STRING,
+  })
+  name!: string;
+
+  @Column({
+    type: DataType.STRING,
+  })
+  description!: string;
+
+  @HasMany(() => UserDepartment)
+  departments!: UserDepartment[];
+}
